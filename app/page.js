64955761346c6d207112c48645b344623409680c
Oluwafisayo <adeyekunoluwafisayo01@gmail.com>
2026-02-11 04:37:00 -0500

    re-render changes

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Technologies from "./_components/Technologies";
 
 const page = () => {
   return (
-    <div className="text-white flex flex-col lg:flex-row lg:justify-between items-start mt-4 gap-4 px-4 lg:px-8 text-justify">
+    <div className="text-white flex flex-col lg:flex-row lg:justify-between items-start mt-4 gap-4 px-4 lg:px-8">
       <div className="w-full lg:w-1/2 lg:sticky lg:top-4 lg:self-start">
         <Introduction />
       </div>
